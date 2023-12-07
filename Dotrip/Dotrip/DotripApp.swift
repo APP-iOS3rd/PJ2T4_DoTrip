7c0afb31b269e2e8676f333f6b912dc46af550f0
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-12-07 17:45:34 +0900

    프로젝트 파일 생성

---
+++ Dotrip/Dotrip/DotripApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotripApp.swift
+//  Dotrip
+//
+//  Created by 이현호 on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DotripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
