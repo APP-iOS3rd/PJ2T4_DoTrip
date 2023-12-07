412ed033c222bf18b20b49645f5f592bd182d2ea
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-12-07 18:06:40 +0900

    테스트뷰 생성

---
+++ Dotrip/Dotrip/Views/testView.swift
@@ -0,0 +1,18 @@
+//
+//  testView.swift
+//  Dotrip
+//
+//  Created by 이현호 on 12/7/23.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testView()
+}
