d9d87b0c2cfe005491663a101a1c29115081e710
youjiin0325 <youjiin0325@gmail.com>
2023-12-11 17:36:55 +0900

    Feat:  ViewModifier 디자인 따로 만듬
    미션 카운트 디자인 따로 만들어서 뺌

--- Dotrip/Dotrip/Views/MyMission/MyMissionList.swift
+++ Dotrip/Dotrip/Views/MyMission/MyMissionList.swift
@@ -10,25 +10,23 @@ import SwiftUI
 struct MyMissionList: View {
     @Binding var mission: Mission
     var body: some View {
-        HStack{
+   
             VStack(alignment:.leading, spacing: 0){
             
                 Text("\(mission.name)")
-                    .font(.system(size: 18))
+                    .font(.system(size: 16))
                     .fontWeight(.bold)
                 
                 Text(mission.address)
-                    .font(.system(size: 15))
+                    .font(.system(size: 14))
                     .fontWeight(.regular)
 
             }
             //피그마에서 지정한 사이즈와 swiftui 에서의 width가 다른듯
-            .frame(width: 300, height: 70)
+            .frame(width: 320, height: 70)
             .background(Color.white)
             .cornerRadius(20)
             .shadow(radius:4, x:0, y:4)
-        }
-        .padding(5 )
-       
+
     }
 }
--- Dotrip/Dotrip/Views/MyMission/MyMissionListView.swift
+++ Dotrip/Dotrip/Views/MyMission/MyMissionListView.swift
@@ -3,13 +3,16 @@
 //  Dotrip
 //
 //  Created by 유지인 on 12/11/23.
-//
+/*
+ 0xEFECEC : 서브컬러
+ */
 
 import SwiftUI
 
 struct MyMissionListView: View {
     @StateObject var missionStore = MissionStore(missions: missionData)
-  
+     var succeededMissons: Int = 0 //성공한 미션 카운트
+
     var body: some View {
         NavigationStack{
             VStack {
@@ -20,27 +23,52 @@ struct MyMissionListView: View {
                     Text("개 있습니다")
                     
                 }
-                .frame(width: 300, height: 30)
-                .background(Color.gray)
-                .cornerRadius(20)
-                .offset(x:0, y:40)
-                
+                .modifier(CountModify())
+        
                 List{
                     ForEach(0..<missionStore.missions.count, id:\.self) { item in
                             NavigationLink(destination:
 MyMissionDetailView(missions: $missionStore.missions[item])) {
                                 MyMissionList(mission: $missionStore.missions[item])
                         }.listRowSeparator(.hidden)
+                        
                     }
                  
                 }
                  .scrollContentBackground(.hidden)
-                
-             .padding()
+
+                //완료된 갯수가 0개 이상이면 나타남
+                if succeededMissons > 0 {
+                    HStack {
+                        Text("진행중인 미션이")
+                        Text("\(succeededMissons)")
+                            .foregroundStyle(Color.orange)
+                        Text("개 있습니다")
+                    }
+                    .modifier(CountModify())
+                    .offset(x:0, y: -300)
+                }
+
             }
         }
     }
 }
+
+
+
+
+//진행중인 미션 갯수 디자인
+struct CountModify: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 350, height: 30)
+            .background(Color(hex: 0xEFECEC))
+            .cornerRadius(20)
+            .offset(x:0, y:40)
+            .padding(.bottom, 20)
+    }
+}
+
 
 #Preview {
     MyMissionListView()
