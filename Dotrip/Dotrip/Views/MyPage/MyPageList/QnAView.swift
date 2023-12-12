7c5b91d01b0e997675fd4bdbcf7fbb3333b7fb34
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-12-12 17:36:03 +0900

    Q&A, 이메일문의 끝

--- Dotrip/Dotrip/Views/MyPage/MyPageList/QnAView.swift
+++ Dotrip/Dotrip/Views/MyPage/MyPageList/QnAView.swift
@@ -7,11 +7,73 @@
 
 import SwiftUI
 
+
+
 struct QnAView: View {
+    struct ListItem: Identifiable {
+        var id: Int
+        var title: String
+        var content: String
+        var isExpanded: Bool = false
+    }
+    
+    @State private var listItems: [ListItem] = [
+        ListItem(id: 0, title: "Q. DoTrip은 뭔가요?", content: "‘DoTrip’은 사용자가 다양한 지역의 관광 명소를 여행하면서 재미있는 미션을 수행하고, 이로 인해 특별한 경험과 보상을 얻을 수 있는 서비스입니다."),
+        ListItem(id: 1, title: "Q. 포인트는 어떻게 사용 하나요?", content: "미션 수행을 통해 포인트를 얻어 '포인트교환'에서 각종 혜택과 보상을 얻을 수 있습니다."),
+        ListItem(id: 2, title: "Q. 미션 수행 완료는 어떻게 하나요?", content: "미션시작 버튼을 눌러 미션을 진행하고, 주어진 미션을 완수하면 자동적으로 완료됩니다.")
+    ]
+    
     var body: some View {
-        Text("자주 묻는 질문 뷰")
+        NavigationStack {
+            List(listItems) { listItem in
+                ListItemView(listItem: listItem, onTap: {
+                    if let index = listItems.firstIndex(where: { $0.id == listItem.id }) {
+                        listItems[index].isExpanded.toggle()
+                    }
+                })
+//                .listRowSeparator(.hidden)
+            }
+            .padding(.top, 30)
+            .listStyle(.plain)
+            .navigationTitle("자주 묻는 질문")
+        }
     }
 }
+
+struct ListItemView: View {
+    var listItem: QnAView.ListItem
+    var onTap: () -> Void
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(listItem.title)
+                    .padding(.leading, 10)
+                Spacer()
+                Image(systemName: "chevron.down")
+                    .foregroundStyle(Color(hex: 0xCCCCCC))
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                onTap()
+            }
+            
+            if listItem.isExpanded {
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: 0xF4F4F4))
+                        .shadow(radius: 2, x: 0, y: 2)
+                    
+                    Text(listItem.content)
+                        .foregroundColor(Color(hex: 0x777777))
+                        .padding()
+                }
+                .padding()
+            }
+        }
+    }
+}
+
 
 #Preview {
     QnAView()
--- Dotrip/Dotrip/Views/MyPage/MyPageView.swift
+++ Dotrip/Dotrip/Views/MyPage/MyPageView.swift
@@ -22,114 +22,23 @@ struct MyPageView: View {
     @ObservedObject private var userData: UserData = UserData()
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             GeometryReader { geometry in
                 VStack {
                     Spacer()
                     
-                    HStack {
-                        Spacer()
-                        
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.white)
-                                .frame(width: geometry.size.width * 0.85, height: geometry.size.width * 0.5)
-                                .shadow(radius: 4, x: 0, y: 4)
-                                .overlay(
-                                    HStack {
-                                        // 좌측 내용
-                                        ZStack(alignment: .topTrailing) {
-                                            // 프로필수정
-                                            Button {
-                                                self.showModal = true
-                                            } label: {
-                                                Image(systemName: "gearshape")
-                                                    .font(.system(size: 20))
-                                                    .foregroundColor(.black)
-                                            }
-                                            .sheet(isPresented: self.$showModal) {
-                                                ModifyProfileView(userData: userData)
-                                            }
-                                            .padding()
-                                            
-                                            VStack {
-                                                Circle()
-                                                    .fill(Color(hex: 0xDADADA))
-                                                    .overlay(
-                                                        Image("capybara")
-                                                            .resizable()
-                                                            .scaledToFit()
-                                                            .frame(width: 70, height: 70)
-                                                    )
-                                                    .padding()
-                                                
-                                                HStack {
-                                                    Text(userData.myPageNickname)
-                                                        .foregroundColor(.black)
-                                                        .bold()
-                                                    Text("님")
-                                                        .foregroundColor(.black)
-                                                }
-                                            }
-                                            .padding()
-                                        .frame(width: geometry.size.width * 0.425)
-                                        }
-                                        
-                                        // 세로 구분선
-                                        Divider()
-                                            .frame(width: 1, height: geometry.size.width * 0.4)
-//                                            .background(Color(hex: 0xDADADA))
-                                        
-                                        Spacer()
-                                        
-                                        // 우측 내용
-                                        VStack {
-                                            Text("보유 포인트")
-                                                .foregroundColor(.black)
-                                                .padding(1)
-                                            Text("1,203")
-                                                .font(.system(size: 30))
-                                                .bold()
-                                                .foregroundColor(Color(hex: 0xFFA800))
-                                        }
-                                        .padding()
-                                        .frame(width: geometry.size.width * 0.425)
-                                    }
-                                )
-                        }
-                        Spacer()
-                    }
-                    .padding()
+                    CardContent(showModal: $showModal, userData: userData, geometry: geometry)
+                        .padding()
                     
                     Spacer()
                     
-                    // 리스트
-                    List {
-                        Section(header: Text("포인트")) {
-                            NavigationLink(destination: PointHistoryView()) {
-                                Text("포인트 사용내역")
-                            }
-                            NavigationLink(destination: PointExchangeView()) {
-                                Text("포인트 교환")
-                            }
-                        }
-                        
-                        Section(header: Text("고객센터")) {
-                            NavigationLink(destination: QnAView()) {
-                                Text("자주 묻는 질문")
-                            }
-                            NavigationLink(destination: EmailInquiryView()) {
-                                Text("이메일 문의")
-                            }
-                        }
-                    }
-                    
-                    .padding()
-                    .listStyle(InsetListStyle())
+                    ListSection()
+                        .padding()
+                        .listStyle(InsetListStyle())
                     
                     HStack {
                         Button {
-                            print("Button action")
+                            print("로그아웃")
                         } label: {
                             Text("로그아웃")
                                 .font(.system(size: 12))
@@ -137,14 +46,14 @@ struct MyPageView: View {
                                 .foregroundStyle(.black)
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 10.0)
-                                        .stroke(Color(hex: 0xC2BDBD),lineWidth: 1.5)
+                                        .stroke(Color(hex: 0xC2BDBD), lineWidth: 1.5)
                                 )
                         }
                         
                         Spacer()
                         
                         Button {
-                            print("Button action")
+                            print("탈퇴하기")
                         } label: {
                             Text("탈퇴하기")
                                 .font(.system(size: 12))
@@ -152,18 +61,126 @@ struct MyPageView: View {
                                 .foregroundStyle(.black)
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 10.0)
-                                        .stroke(Color(hex: 0xC2BDBD),lineWidth: 1.5)
+                                        .stroke(Color(hex: 0xC2BDBD), lineWidth: 1.5)
                                 )
                         }
                     }
                     .padding(40)
                 }
-            } // End VStack
-            
+            }
         }
-        .tabItem {
-            Image(systemName: "person")
-            Text("MY")
+    }
+}
+
+// 프로필 정보
+struct CardContent: View {
+    @Binding var showModal: Bool
+    @ObservedObject var userData: UserData
+    var geometry: GeometryProxy
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+                .frame(width: geometry.size.width * 0.85, height: geometry.size.width * 0.5)
+                .shadow(radius: 4, x: 0, y: 4)
+                .overlay(
+                    HStack {
+                        LeftContent(userData: userData, showModal: $showModal, geometry: geometry)
+                        Divider()
+                            .frame(width: 1)
+                        RightContent(geometry: geometry)
+                    }
+                )
+        }
+    }
+}
+
+// 프로필 왼쪽 정보
+struct LeftContent: View {
+    var userData: UserData
+    @Binding var showModal: Bool
+    var geometry: GeometryProxy
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Button {
+                self.showModal = true
+            } label: {
+                Image(systemName: "gearshape")
+                    .font(.system(size: 20))
+                    .foregroundColor(.black)
+            }
+            .sheet(isPresented: self.$showModal) {
+                ModifyProfileView(userData: userData)
+            }
+            .padding()
+            
+            VStack {
+                Circle()
+                    .fill(Color(hex: 0xDADADA))
+                    .overlay(
+                        Image("capybara")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 70, height: 70)
+                    )
+                    .padding()
+                
+                HStack {
+                    Text(userData.myPageNickname)
+                        .foregroundColor(.black)
+                        .bold()
+                    Text("님")
+                        .foregroundColor(.black)
+                }
+            }
+            .padding()
+            .frame(width: geometry.size.width * 0.425)
+        }
+    }
+}
+
+// 오른쪽 정보
+struct RightContent: View {
+    var geometry: GeometryProxy
+    
+    var body: some View {
+        VStack {
+            Text("보유 포인트")
+                .foregroundColor(.black)
+                .padding(1)
+            Text("1,203")
+                .font(.system(size: 30))
+                .bold()
+                .foregroundColor(Color(hex: 0xFFA800))
+        }
+        .padding()
+        .frame(width: geometry.size.width * 0.425)
+    }
+}
+
+// 리스트
+struct ListSection: View {
+    var body: some View {
+        List {
+            Section(header: Text("포인트")) {
+                NavigationLink(destination: PointHistoryView()) {
+                    Text("포인트 사용내역")
+                }
+                NavigationLink(destination: PointExchangeView()) {
+                    Text("포인트 교환")
+                }
+            }
+            
+            Section(header: Text("고객센터")) {
+                NavigationLink(destination: QnAView()) {
+                    Text("자주 묻는 질문")
+                }
+                NavigationLink(destination: EmailInquiryView()) {
+                    Text("이메일 문의")
+                }
+            }
         }
     }
 }
