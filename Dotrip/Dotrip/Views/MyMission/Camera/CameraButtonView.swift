7436edd9fefea109ab4ba12f0ff1f15a43d114e0
youjiin0325 <youjiin0325@gmail.com>
2023-12-13 12:07:42 +0900

    Refactor: Button코드 가독성을 위해 변경, 미션 시작 버튼 클릭 후 미션포기 버튼 나타나기 (수정완료)

--- Dotrip/Dotrip/Views/MyMission/Camera/CameraButtonView.swift
+++ Dotrip/Dotrip/Views/MyMission/Camera/CameraButtonView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 유지인 on 12/11/23.
 //
-
+//
 import SwiftUI
 
 struct CameraButtonView: View {
@@ -35,77 +35,42 @@ struct CameraButtonView: View {
                     
                 }
             }else{
-                Button(action: {
-                    self.showActionSheet.toggle()
-                    btnStatus = true
-                }){
+                VStack{
+                    Rectangle()
+                        .foregroundColor(.gray)
+                        .opacity(0.1)
+                        .frame(width: 330,height: 350)
+                        .cornerRadius(20)
+                        .offset(x:0,y: -40)
                     
-                    Text("미션중")
+                    Text("미션 중")
                         .fontWeight(.regular)
                         .frame(width:250, height: 50)
                         .foregroundColor(.white)
                         .background(Color.orange)
                         .cornerRadius(10)
                         .shadow(radius:2, x:0, y:2)
-                }.offset(x:0,y: 180)
+                    
+                }
+//            Button(action: {
+//               
+//            }) {
+//                Text("미션 포기하기")
+//                    .fontWeight(.regular)
+//                    .frame(width:250, height: 50)
+//                    .foregroundColor(.black)
+//                    .background(Color.white)
+//                    .cornerRadius(10)
+//                    .shadow(radius:2, x:0, y:2)
+//            }.offset(x:0,y: 40)
             }
         }
     }
         
-        
-        
-        
-//        Button(action: {
-//            self.showActionSheet.toggle()
-//            btnStatus = true
-//        }) {
-//            if btnStatus == false {
-//                VStack{
-//                    Image("경복궁사진1")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .frame(width: 350, height: 400)
-//                        .cornerRadius(20)
-//                        .padding(.vertical,40)
-//                    
-//                        Text("미션 시작")
-//                            .fontWeight(.regular)
-//                            .frame(width:250, height: 50)
-//                            .foregroundColor(.black)
-//                            .background(Color.white)
-//                            .cornerRadius(10)
-//                            .shadow(radius:4, x:0, y:4)
-//                      
-//             
-//                }.offset(x:0, y: -290)
-//                   
-//            }else {
-//                VStack{
-//                    Text("미션완료")
-//                        .fontWeight(.regular)
-//                        .frame(width:250, height: 50)
-//                        .foregroundColor(.white)
-//                        .background(Color.orange)
-//                        .cornerRadius(10)
-//                        .shadow(radius:4, x:0, y:4)
-//                        .offset(x:0, y: -45)
-//                    
-//                }
-//
-//            }
-//            
-//        }
-
-       
-    }
+}
  
 
 #Preview { CameraButtonView(showActionSheet: .constant(false))
        .previewLayout(.sizeThatFits)
      
 }
-
-
-//#Preview {
-//    MyMissionCameraView()
-//}
