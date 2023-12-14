1780872681e21f6e932c178789eea946780af1cc
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-12-14 10:18:37 +0900

    구매확인 알림

---
+++ Dotrip/Dotrip/Models/ProductDetailModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailModel.swift
+//  Dotrip
+//
+//  Created by 이현호 on 12/14/23.
+//
+
+import SwiftUI
+
+struct ProductDetailModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailModel()
+}
