7a588feb5e33229d1232483cecece77e4b88bd12
youjiin0325 <youjiin0325@gmail.com>
2023-12-14 20:40:32 +0900

    버튼 수정

--- Dotrip/Dotrip/Views/MyMission/Camera/CameraButtonView.swift
+++ Dotrip/Dotrip/Views/MyMission/Camera/CameraButtonView.swift
@@ -5,43 +5,43 @@
 //  Created by 유지인 on 12/11/23.
 //
 
-import SwiftUI
-
-struct CameraButtonView: View {
-    @Binding var showActionSheet : Bool
-    @Binding var btnStatus :Bool
-    
-    var body: some View {
-            Button {
-                self.showActionSheet.toggle()
-                btnStatus = true
-            } label: {
-                ZStack{
-                    Rectangle()
-                        .foregroundColor(.black)
-                        .font(.system(size: 80))
-                        .frame(width: 330,height: 350)
-                        .background(Color.gray)
-                        .cornerRadius(20)
-                        .opacity(0.1)
-                    Text("여기를 클릭 해주세요!")
-                        .font(.subheadline)
-                        .foregroundStyle(Color.gray)
-                        .fontWeight(.medium)
-                        .opacity(0.7)
-                        .offset(x:0,y: 40)
-                    
-                    Image(systemName:"camera")
-                        .foregroundColor(.black)
-                        .font(.system(size: 50))
-                        .opacity(0.1)
-                }
-        }
-    }
-}
-    
-
-#Preview {
-    CameraButtonView(showActionSheet: .constant(false), btnStatus: .constant(true))
-}
-
+//import SwiftUI
+//
+//struct CameraButtonView: View {
+//    @Binding var showActionSheet : Bool
+//    @Binding var btnStatus :Bool
+//    
+//    var body: some View {
+//            Button {
+//                self.showActionSheet.toggle()
+//                btnStatus = true
+//            } label: {
+//                ZStack{
+//                    Rectangle()
+//                        .foregroundColor(.black)
+//                        .font(.system(size: 80))
+//                        .frame(width: 330,height: 350)
+//                        .background(Color.gray)
+//                        .cornerRadius(20)
+//                        .opacity(0.1)
+//                    Text("여기를 클릭 해주세요!")
+//                        .font(.subheadline)
+//                        .foregroundStyle(Color.gray)
+//                        .fontWeight(.medium)
+//                        .opacity(0.7)
+//                        .offset(x:0,y: 40)
+//                    
+//                    Image(systemName:"camera")
+//                        .foregroundColor(.black)
+//                        .font(.system(size: 50))
+//                        .opacity(0.1)
+//                }
+//        }
+//    }
+//}
+//    
+//
+//#Preview {
+//    CameraButtonView(showActionSheet: .constant(false), btnStatus: .constant(true))
+//}
+//
