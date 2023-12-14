c9f567b421ef233b89e0faa85f06333ac37931e3
youjiin0325 <youjiin0325@gmail.com>
2023-12-14 10:47:55 +0900

    Feat: 미션 버튼 문구 수정, 미션 완료 후 알림창 생성 * 미션수행중 -> 미션 수행하기 *미션 수행하기 버튼 클릭 -> 알림창으로 "미션 완료 표시"

--- Dotrip/Dotrip/Views/MyMission/MyMissionDetailView.swift
+++ Dotrip/Dotrip/Views/MyMission/MyMissionDetailView.swift
@@ -12,61 +12,124 @@ struct MyMissionDetailView: View {
     @StateObject var missionStore : MissionStore
     @Binding var missions : Mission
     @Binding var path: NavigationPath
+    @Binding var succeededMissons: Int
     
+    //카메라
     @State var showActionSheet: Bool = false
     @State var showImagePicker: Bool = false
     @State var image : Image?
     @State var sourcetype :Int = 0
     
     @State var btnStatus :Bool = false
+    @State var showingAlert: Bool  = false
+    
     
     var body: some View {
-        ZStack{
-            VStack {
+        ZStack(alignment:.center){
+           
+            VStack(alignment:.leading, spacing: 0){
+                Spacer()
                 VStack(alignment:.leading, spacing: 0){
                     Text("\(missions.name)")
                         .font(.system(size: 20))
                         .fontWeight(.bold)
-                        .padding(.bottom,8)
+                        .padding(.bottom,7)
                     Text("미션내용:\(missions.description)")
                         .font(.system(size: 15))
                         .fontWeight(.regular)
                     Text("주소: \(missions.address)")
                         .font(.system(size: 15))
                         .fontWeight(.regular)
-                }.offset(x:0,y:-80)
-                
-                VStack{
                     
-                    if image != nil {
-                        
-                        image?
+                }.position(x: 195 , y:60)
+             
+               
+                if btnStatus == false {
+                    VStack{
+                        Image("경복궁사진1")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .frame(width: 330,height: 350)
                             .cornerRadius(20)
-                            .offset(x:0, y:-70)
-                            .overlay(
-                                CameraButtonView(showActionSheet: $showActionSheet)
+                            .padding(.bottom,30)
+                        
+                        Button {
+                            btnStatus = true
+                        } label: {
+                            Text("미션 시작하기")
+                                .fontWeight(.regular)
+                                .frame(width:250, height: 50)
+                                .foregroundColor(.black)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .shadow(radius:2, x:0, y:2)
+                        }
+                    }.position(x: 195 , y:15)
+                }
+                    else {
+                        VStack{
+                            image?
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 330,height: 350)
+                                .cornerRadius(20)
+                                .position(x: 195 , y:-30)
+                                .overlay(
+                                    CameraButtonView(showActionSheet: $showActionSheet,btnStatus: $btnStatus)
+                                        .position(x: 195 , y:-32))
+                             }
+                        }
+                  }
+            
+            
+                Spacer()
+                VStack{
+                   
+                    if btnStatus {
+                        Button {
+                            showingAlert = true
+                            succeededMissons += 1
+                        } label: {
+                            Text("미션 수행하기")
+                                .fontWeight(.regular)
+                                .frame(width: 250, height: 50)
+                                .foregroundColor(.white)
+                                .background(Color(hex: 0xFFA800))
+                                .cornerRadius(10)
+                                .shadow(radius: 2, x: 0, y: 2)
+                        }
+                        .alert(isPresented: $showingAlert) {
+                            Alert(
+                                title: Text("미션 완료"),
+                                message: Text("미션이 완료되었습니다."),
+                                dismissButton: .default(Text("확인")) {
+                                    giveupMission()
+                                }
                             )
+                        }.position(x: 195 , y:550)
+                            
                     }
                     
-                    Button(action: {
-                        giveupMission()
-                        print("\(showActionSheet)")
-                        print("\(showImagePicker)")
-                    }) {
-                        Text("미션 포기하기")
-                            .fontWeight(.regular)
-                            .frame(width:250, height: 50)
-                            .foregroundColor(.black)
-                            .background(Color.white)
-                            .cornerRadius(10)
-                            .shadow(radius:2, x:0, y:2)
-                    }.offset(x:0,y: 30)
+                    
+                    if btnStatus {
+                        Button {
+                            giveupMission()
+                        } label: {
+                            Text("미션 포기하기")
+                                .fontWeight(.regular)
+                                .frame(width:250, height: 50)
+                                .foregroundColor(.black)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .shadow(radius:2, x:0, y:2)
+                        }.position(x: 195 , y:270)
+                    }
                 }
-            }
-    
+
+            
+            
+            
+            //카메라 로직부분
             .actionSheet(isPresented: $showActionSheet, content:{ () -> ActionSheet in
                 ActionSheet(title: Text("이미지 선택하기"),message: Text("사진첩에서 이미지를 고르거나 카메라를 사용해주세요"), buttons: [ActionSheet.Button.default(Text("카메라"), action: {
                     self.sourcetype = 0
@@ -86,7 +149,8 @@ struct MyMissionDetailView: View {
         .onAppear{self.image = Image(self.missions.image) }
     }
 
- 
+    
+ //네비게이션 패스
     func giveupMission() {
         guard !path.isEmpty else {
             print("Path is empty!")
@@ -95,5 +159,6 @@ struct MyMissionDetailView: View {
         print("Removing from path: \(path)")
         path.removeLast()
     }
+
 }
 
--- Dotrip/Dotrip/Views/MyMission/MyMissionList.swift
+++ Dotrip/Dotrip/Views/MyMission/MyMissionList.swift
@@ -34,3 +34,30 @@ struct MyMissionList: View {
     }
 }
 
+struct SucceededMisson: View {
+    var body: some View {
+   
+            VStack(alignment:.leading){
+                Text("경복궁에서 한복을 입고 인증샷 찍기")
+                    .font(.system(size: 18))
+                    .fontWeight(.bold)
+                    .padding(.vertical,0.5)
+                    .offset(x:-12,y:0)
+                Text("서울특별시 종로구 사직로 161")
+                    .font(.system(size: 15))
+                    .fontWeight(.regular)
+                    .offset(x:-12,y:0)
+                  
+
+            }
+            
+            .frame(width: 320, height: 70)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius:2, x:0, y:2)
+          
+
+    }
+}
+
+
